7cd390a5b9007095bc778f5a9de0247231300cfe
Sadashiv Gawade <gawadelaxmi075@gmail.com>
2026-08-21 11:39:38 +0530

    Time: 2 ms (81.05%), Space: 11.9 MB (10.56%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,55 +2,61 @@ class Solution {
 public:
     string minWindow(string s, string t) {
 
-        if (t.size() > s.size())
+        int n = s.size();
+        int m = t.size();
+
+        if (m > n)
             return "";
 
-        unordered_map<char, int> mp;
+        vector<int> need(256, 0);
+        vector<int> have(256, 0);
 
-        // Frequency required from t
+        // Store frequency of characters required from t
         for (char ch : t) {
-            mp[ch]++;
+            need[ch]++;
         }
 
         int low = 0;
         int high = 0;
 
-        int count = t.size();
-
+        int count = 0;          // characters matched
         int minLen = INT_MAX;
-        int start = 0;
+        int start = -1;
 
-        while (high < s.size()) {
+        while (high < n) {
 
-            // Character enters the window
-            if (mp[s[high]] > 0) {
-                count--;
+            // Add s[high] to current window
+            have[s[high]]++;
+
+            // If this character is still needed
+            if (have[s[high]] <= need[s[high]]) {
+                count++;
             }
 
-            mp[s[high]]--;
-            high++;
+            // Window is valid
+            while (count == m) {
 
-            // Window contains all characters
-            while (count == 0) {
+                int len = high - low + 1;
 
                 // Update minimum window
-                if (high - low < minLen) {
-                    minLen = high - low;
+                if (len < minLen) {
+                    minLen = len;
                     start = low;
                 }
 
-                // Remove left character
-                mp[s[low]]++;
-
-                if (mp[s[low]] > 0) {
-                    count++;
+                // Remove s[low]
+                if (have[s[low]] <= need[s[low]]) {
+                    count--;
                 }
 
+                have[s[low]]--;
                 low++;
             }
+
+            high++;
         }
 
-        if (minLen == INT_MAX)
+        if (start == -1)
             return "";
 
         return s.substr(start, minLen);
